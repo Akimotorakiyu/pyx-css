d14e99e51edffed8aebe1f5403669b0cef435980
湫曗 <autumnsilverleaves@gmail.com>
2021-03-21 23:53:56 +0800

    add effect

---
+++ src/App.vue
@@ -0,0 +1,105 @@
+<template>
+  <div id="app">
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+    <div style="">
+      <el-table
+        :data="tableData"
+        style="width: 100%"
+        row-class-name="custom-row-class"
+      >
+        <el-table-column prop="date" label="日期" width="180">
+        </el-table-column>
+        <el-table-column prop="name" label="姓名" width="180">
+        </el-table-column>
+        <el-table-column prop="address" label="地址"> </el-table-column>
+        <el-table-column fixed="right" label="操作" width="100">
+          <template slot-scope="scope">
+            <div class="opertion">
+              <el-button
+                @click="handleClick(scope.row)"
+                type="text"
+                size="small"
+                >查看</el-button
+              >
+              <el-button type="text" size="small">编辑</el-button>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "App",
+  components: {},
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+    };
+  },
+});
+</script>
+
+<style lang="stylus" scoped>
+
+.opertion{
+  opacity 0
+  transition-property all
+  transition-duration 100ms
+  transition-timing-function ease-in-out
+  text-align center
+
+  .el-button{
+    font-size 10px
+    transition-property all
+    transition-duration 100ms
+    transition-timing-function ease-in-out
+  }
+}
+
+ .hover-row .opertion{
+  opacity 1
+
+  .el-button{
+    font-size 12px
+  }
+}
+</style>
+
+<style lang="stylus">
+html {
+  font-family -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+}
+</style>
